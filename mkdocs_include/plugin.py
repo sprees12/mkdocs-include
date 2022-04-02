ad56468972071c49c2fc7e34fe49677b47fbe2eb
sprees12 <78652752+sprees12@users.noreply.github.com>
2022-04-03 01:10:32 +0900

    addd encoding="utf-8"

--- mkdocs_include/plugin.py
+++ mkdocs_include/plugin.py
@@ -15,7 +15,7 @@ class IncludePlugin(BasePlugin):
         # !!! TODO support BOF, EOF markers
         # !!! TODO support line range
         path = f'{self.config["src_path"]}/{filename}'
-        with open(path, 'r') as f:
+        with open(path, 'r', encoding="utf-8") as f:
             return f.read()
 
     def on_page_markdown(self, markdown, page, config, **kwargs):
